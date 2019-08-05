307f46b653590fd845dad960d8af7df5e7247546
ogiogi93 <takashi-ogiwara@haveagood.holiday>
2019-08-05 15:47:51 +0900

    Downgrade digdag and delete postgresql

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-server() {
+scheduler() {
   envsubst < config.template.yml > config.yml.tmp
   mv config.yml.tmp config.yml
 
-  exec java -jar /usr/local/bin/digdag server -m -b 0.0.0.0 -P config.yml -X io.digdag.limits.maxWorkflowTasks=100000 -c allow.properties --log /var/lib/digdag/logs/server --task-log /var/lib/digdag/logs/tasks
+  exec java -jar /usr/local/bin/digdag scheduler -m -b 0.0.0.0 -P config.yml -X io.digdag.limits.maxWorkflowTasks=100000 -c allow.properties --log /var/lib/digdag/logs/server --task-log /var/lib/digdag/logs/tasks
 }
 
 case "$1" in
-  server)
+  scheduler)
     shift
-    server
+    scheduler
     ;;
   *)
     exec "$@"
